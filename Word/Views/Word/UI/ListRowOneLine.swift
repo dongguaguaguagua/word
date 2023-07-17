2f6346bd46a1f78def90a50e164273d513ea5d36
nanguaguag <2022181640245@stu.scu.edu.cn>
2023-07-17 12:26:40 +0800

    beautify the one line list row

--- Word/Views/Word/UI/ListRowOneLine.swift
+++ Word/Views/Word/UI/ListRowOneLine.swift
@@ -9,28 +9,34 @@ import SwiftUI
 
 struct ListRowOneLine: View {
     @EnvironmentObject var ModelData: ModelDataClass
-    
+
     @Binding var isShowEnglish: Bool
     @Binding var isShowChinese: Bool
-    
+
     /// whether show Chinese or English individually
     @State var isShowEnglishSingle: Bool=false
     @State var isShowChineseSingle: Bool=false
-    
+
     /// receive a word
     var word: singleWord
-    
+
     var body: some View {
         HStack {
             HStack {
-                Text("\(getText(input: "chinese"))")
-                    .onTapGesture {
+                if showBtn(isChinese: true) {
+                    Button(action: {
                         isShowChineseSingle.toggle()
+                    }) {
+                        Image(systemName: "poweroff")
+                            .font(.system(size: 15))
+                            .foregroundColor(.red)
                     }
+                    .buttonStyle(BorderlessButtonStyle())
+                }
                 Text(word.name)
                     .font(.title3)
                     /// The hide action is realized by modifying the color.(`Color.clear`)
-                    .foregroundColor((!isShowChinese)||isShowChineseSingle ? Color.black : Color.clear)
+                    .foregroundColor((!isShowChinese) || isShowChineseSingle ? Color.black : Color.clear)
                     /// ease in and out animation
                     .animation(.easeInOut(duration: 0.2), value: isShowChineseSingle)
                     .lineLimit(1)
@@ -43,29 +49,31 @@ struct ListRowOneLine: View {
                     .foregroundColor((!isShowEnglish) || isShowEnglishSingle ? Color.gray : Color.clear)
                     .animation(.easeInOut(duration: 0.2), value: isShowEnglishSingle)
                     .lineLimit(1)
-//                Spacer()
-                Text("\(getText(input: "english"))")
-                    .onTapGesture {
+                if showBtn(isChinese: false) {
+                    Button(action: {
                         isShowEnglishSingle.toggle()
+                    }) {
+                        Image(systemName: "poweroff")
+                            .font(.system(size: 15))
+                            .foregroundColor(.red)
                     }
+                    .buttonStyle(BorderlessButtonStyle())
+                }
             }
         }
     }
 
-    func getText(input: String) -> String {
+    func showBtn(isChinese: Bool) -> Bool {
         if !isShowEnglish && !isShowChinese {
-            return ""
-        } else if input=="chinese" && !isShowChinese {
-            return ""
-        } else if input=="chinese" && isShowChinese {
-            return "◯"
-        } else if input=="english" && !isShowEnglish {
-            return ""
-        } else if input=="english" && isShowEnglish {
-            return "◯"
-        } else {
-            /// This situation would never appear.
-            return "unknown"
+            return false
         }
+        if isChinese && !isShowChinese {
+            return false
+        }
+        if !isChinese && !isShowEnglish {
+            return false
+        }
+
+        return true
     }
 }
